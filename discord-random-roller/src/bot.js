c061b7ea1f0bb150242ed70ac5b36e3351448457
Gustavo Rodarte <gustavorodarte@Gustavos-MacBook-Pro.local>
2023-08-09 12:40:44 -0400

    draft the migration to slash comands discord api

--- discord-random-roller/src/bot.js
+++ discord-random-roller/src/bot.js
@@ -1,17 +1,24 @@
 /* eslint-disable fp/no-unused-expression */
-const isTruthy = require('crocks/predicates/isTruthy');
-const Eris = require('eris');
-const diceRoller = require('./diceRoller');
-const randomService = require('./services/random');
+const isTruthy = require("crocks/predicates/isTruthy");
+const diceRoller = require("./diceRoller");
+const randomService = require("./services/random");
+const nacl = require("tweetnacl");
 
-const ERROR_MSG = 'Ops, parece que tivemos um problema. Os siderais já foram chamados, por favor agaurde e tente novamente.';
+// Create a new client instance
+
+const ERROR_MSG =
+  "Ops, parece que tivemos um problema. Os siderais já foram chamados, por favor aguarde e tente novamente.";
 
 const parseRoll = (diceRoll, rollResult) => {
   const hasTargetNumber = isTruthy(rollResult.targetNumber);
-  const msg = `\`${diceRoll}\` = ${rollResult.dicesValues.join(', ')} = **${rollResult.success} sucessos**`;
+  const msg = `\`${diceRoll}\` = ${rollResult.dicesValues.join(", ")} = **${
+    rollResult.success
+  } sucessos**`;
 
   const totalValue = rollResult.dicesValues.reduce((acc, curr) => acc + curr);
-  const msg2 = `\`${diceRoll}\` = ${rollResult.dicesValues.join(', ')} = **${totalValue}**`;
+  const msg2 = `\`${diceRoll}\` = ${rollResult.dicesValues.join(
+    ", "
+  )} = **${totalValue}**`;
   return hasTargetNumber ? msg : msg2;
 };
 
@@ -21,42 +28,95 @@ const makeBotResponse = (bot, msg, rollParsed) => {
   return bot.createMessage(msg.channel.id, message);
 };
 
-const rollDice = (bot, msg) => {
+const rollDice = (body) => {
   const { getRandomIntegers } = randomService();
-  return diceRoller.executeRoll(getRandomIntegers)(msg.content)
-    .map((result) => result.fork(
-      (err) => {
-        console.log('​rollDice -> err', err);
-        return bot.createMessage(msg.channel.id, ERROR_MSG);
-      },
-      (rollResult) => {
-        const rollParsed = parseRoll(msg.content, rollResult);
-        return makeBotResponse(bot, msg, rollParsed);
-      },
-    ));
+  return diceRoller
+    .executeRoll(getRandomIntegers)(msg.content)
+    .map((result) =>
+      result.fork(
+        (err) => {
+          console.log("​rollDice -> err", err);
+          return {
+            type: 4,
+            data: {
+              content: ERROR_MSG,
+            },
+          };          
+        },
+        (rollResult) => {
+          const rollParsed = parseRoll(msg.content, rollResult);
+          return makeBotResponse(bot, msg, rollParsed);
+        }
+      )
+    );
 };
 
-// eslint-disable-next-line fp/no-nil
-const handleMessage = (bot) => (msg) => {
-  try {
-    const shouldRollDice = msg.content.substring(0, 1) === '#';
-    const botResponse = shouldRollDice ? rollDice(bot, msg) : '';
-    return botResponse;
-  } catch (error) {
-    console.error(error);
-    return bot.createMessage(msg.channel.id, ERROR_MSG);
-  }
+const commandsMap = {
+  "roll": rollDice,
 };
 
-const startBot = ({
-  token,
-}) => {
-  const bot = new Eris(token);
-  bot.on('ready', () => console.log('Ready!'));
-  bot.on('messageCreate', handleMessage(bot));
-  return bot.connect();
+const handleMessage = (body) => {
+  const isPing = body.type === 1;
+
+  return isPing ? pongResponse() : commandsMap[body.data.name](body) ;
+}
+
+
+const verifySignature = (signature, timestamp, body) => {
+  const PUBLIC_KEY = process.env.PUBLIC_KEY;
+  const isVerified = nacl.sign.detached.verify(
+    Buffer.from(timestamp + body),
+    Buffer.from(signature, "hex"),
+    Buffer.from(PUBLIC_KEY, "hex")
+  );
+
+  return isVerified;
 };
 
-module.exports = {
-  startBot,
+// Replying to ping (requirement 2.)
+
+// Handle /foo Command
+if (body.data.name == "foo") {
+  return JSON.stringify({
+    // Note the absence of statusCode
+    type: 4, // This type stands for answer with invocation shown
+    data: { content: "bar" },
+  });
+}
+
+return {
+  statusCode: 404, // If no handler implemented for Discord's request
+};
+
+const unauthorizedResponse = () => ({
+  statusCode: 401,
+  body: JSON.stringify("invalid request signature"),
+});
+
+const notFoundResponse = () => ({
+  statusCode: 404,
+});
+
+
+const pongResponse = () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ type: 1 }),
+  };
+};
+
+exports.handler = async (event) => {
+  // Checking signature (requirement 1.)
+  // Your public key can be found on your application in the Developer Portal
+  const PUBLIC_KEY = process.env.PUBLIC_KEY;
+
+  const signature = event.headers["x-signature-ed25519"];
+  const timestamp = event.headers["x-signature-timestamp"];
+  const strBody = event.body; // should be string, for successful sign
+
+  const isVerified = verifySignature(signature, timestamp, strBody);
+
+  const parsedBody = JSON.parse(strBody);
+
+  return isVerified ? await handleMessage(bot)(msg) : unauthorizedResponse();
 };

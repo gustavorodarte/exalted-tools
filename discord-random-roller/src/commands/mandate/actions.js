4f8a91f8edfd86afce2d36f33ffe70c76df21c39
Gustavo Rodarte <gustavo@rodarte.dev>
2024-08-21 15:01:15 +0100

    create a actions file with a draft strcuture of the mandate actions object

---
+++ discord-random-roller/src/commands/mandate/actions.js
@@ -0,0 +1,72 @@
+const actions = {
+  ATTACK: {
+    name: 'Attack',
+    roll: '',
+    baseDifficulty: '',
+  },
+  BEING_INFORMED: {
+    name: 'Being Informed',
+    roll: '',
+    baseDifficulty: '',
+  },
+  ESPIONAGE: {
+    name: 'Espionage',
+    roll: '',
+    baseDifficulty: '',
+  },
+
+  POLICING: {
+    name: 'Policing',
+    roll: '',
+    baseDifficulty: '',
+  },
+  UNCONVENTIONAL_WARFARE: {
+    name: 'Unconventional Warfare',
+    roll: '',
+    baseDifficulty: '',
+  },
+  IMPROVE_THE_CULTURE: {
+    name: 'Improve the Culture',
+    roll: '',
+    baseDifficulty: '',
+  },
+  RISE_IN_RISE_STATURE: {
+    name: 'Rise in Stature',
+    roll: '',
+    baseDifficulty: '',
+  },
+  TRAIN_AND_LEVY_TROOPS: {
+    name: 'Train and Levy Troops',
+    roll: '',
+    baseDifficulty: '',
+  },
+  MAINTAIN_WEALTH: {
+    name: 'Maintain Wealth',
+    roll: '',
+    baseDifficulty: '',
+  },
+  MAINTAIN_INFLUENCE: {
+    name: 'Maintain Influence',
+    roll: '',
+    baseDifficulty: '',
+  },
+  MAINTAIN_REACH: {
+    name: 'Maintain Reach',
+    roll: '',
+    baseDifficulty: '',
+  },
+  MAINTAIN_SOVEREIGNTY: {
+    name: 'Maintain Sovereignty',
+    roll: '',
+    baseDifficulty: '',
+  },
+  MAINTAIN_MILITARY: {
+    name: 'Maintain Military',
+    roll: '',
+    baseDifficulty: '',
+  },
+};
+
+module.exports = {
+  actions,
+};
